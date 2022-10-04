faf544a21e0a112644f710795a15869c9c1286f7
James Perkins <contactme@jamesperkins.dev>
2022-10-04 10:31:26 -0400

    Adding base code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import '/styles/globals.css'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import SignUpPage from './sign-up/[[...index]]'
+import SignInPage from './sign-in/[[...index]]'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const publicPages = ['/', '/sign-in/[[...index]]', '/sign-up/[[...index]]']
+
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  /**
+   * If the current route is listed as public, render it directly.
+   * Otherwise, use Clerk to require authentication.
+   */
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Clerk + Netlify Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      {publicPages.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            {router.pathname.match('/sign-up') ? (
+              <SignUpPage />
+            ) : (
+              <SignInPage />
+            )}
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/user-profile/[[...index]].jsx
@@ -0,0 +1,6 @@
+import { UserProfile } from '@clerk/nextjs'
+
+const UserProfilePage = () => (
+<div className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center"><UserProfile  /></div>)
+
+export default UserProfilePage
