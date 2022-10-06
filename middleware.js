136008028390caa01a539ef3fe31523ab6f5be1a
James Perkins <contactme@jamesperkins.dev>
2022-10-06 19:02:37 -0400

    Fix up middleware
    and remove svgs that aren't used

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
+// Set the paths that don't require the user to be signed in
 const publicPaths = ['/', '/sign-in', '/sign-up']
 
 export default withClerkMiddleware(req => {
